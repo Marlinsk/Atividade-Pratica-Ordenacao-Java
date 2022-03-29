fbbcdaa61ec666d178173b1cd3c6984d1de3ae21
Marlon <Marlonnlmx@outlook.com>
2022-03-28 22:11:06 -0300

    Add comments

--- Atividade-Pratica-Ordenacao-EDRAF1/src/code/Read.java
+++ Atividade-Pratica-Ordenacao-EDRAF1/src/code/Read.java
@@ -1,9 +1,9 @@
 package code;
 
-import java.io.BufferedReader; // faz a leitura da entrada de dados
-import java.io.FileNotFoundException; // tratativa de erro ao não encontrar o arquivo
-import java.io.FileReader; // lê o arquivo
-import java.util.List; // lê um objeto arrayList
+import java.io.BufferedReader; 
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+import java.util.List; 
 
 public class Read {
 	
@@ -11,31 +11,25 @@ public class Read {
 	public static void Readfile(List<Item> Itemlist) throws Exception {
 		try {
 			
-			// Caminho do arquivo JogosDesordenados.csv
 			String path = "C://Users/Marlinsk/git/Atividade-Pratica-Ordenacao-EDRAF1/Atividade-Pratica-Ordenacao-EDRAF1/file/JogosDesordenados.csv";
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			
-			// Armazena os dados do arquivo JogosDesordenados.csv
 			String line = "";
 			
-			// Criando um loop para percorrer os dados do arquivo JogosDesordenados.csv e armazenar em um atributo
 			while ((line = br.readLine()) != null) {
 				
-				// Separa os dados por uma (,)
 				String[] Gameslist = line.split(",");
 				
-				// Instanciando um objeto para obter seus atributos.
 				Item item = new Item();
 				
-				// Armazenando na posição
-				item.game = Gameslist[0]; // Armazena todos os nomes dos jogos 
-				item.category = Gameslist[1]; // Armazena todos os nomes das categorias
-				item.evaluation = Gameslist[2]; // Armazena todas as notas/avaliações dos jogos
+				item.game = Gameslist[0]; 
+				item.category = Gameslist[1]; 
+				item.evaluation = Gameslist[2]; 
 				
 				Itemlist.add(item);
 				
 			}
-		// Para tratativa de erro caso ocorra uma falha ao o arquivo	
+			
 		} catch (FileNotFoundException e) {
 			throw e;
 		}
--- Atividade-Pratica-Ordenacao-EDRAF1/src/code/Write.java
+++ Atividade-Pratica-Ordenacao-EDRAF1/src/code/Write.java
@@ -1,16 +1,15 @@
 package code;
 
-import java.io.FileWriter; // cria um arquivo
-import java.io.IOException; // trata uma exceção de erro do tipo IO
-import java.util.List; // lê um objeto arrayList
-import java.util.stream.Collectors; // implementa várias operações úteis de agregação como contagem, média, soma, máximo, agrupamento e outras que podem ser aplicadas ao final do processamento de uma Stream 
+import java.io.FileWriter;
+import java.io.IOException; 
+import java.util.List; 
+import java.util.stream.Collectors; 
 
 public class Write {
 	
 	// Método para criar o arquivo com os dados modificados
 	public static void writeFile(List<Item> listaItens) throws IOException {
 		
-		// Especificando o caminho onde vai ficar salvo o arquivo criado
         FileWriter writer = new FileWriter("C://Users/Marlinsk/git/Atividade-Pratica-Ordenacao-EDRAF1/Atividade-Pratica-Ordenacao-EDRAF1/file/JogosOrdenados.csv");
         
         String collect =  listaItens.stream().map(item -> item.game + ", " + item.category + ", " + item.evaluation + "\n").collect(Collectors.joining(""));
